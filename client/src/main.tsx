91d29a768ca45ad7f733dd366cc814cc95ee390a
Marcos Joaquin Poet <mpoet@frsf.utn.edu.ar>
2024-11-20 01:59:52 -0300

    "".

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import {ChakraProvider} from "@chakra-ui/react";
-import { ThemeProvider } from 'next-themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from "./chakra/theme.ts";
 
@@ -11,12 +10,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider attribute="class">
             <ChakraProvider value={theme} >
                 <QueryClientProvider client={queryClient}>
                     <App />
                 </QueryClientProvider>
             </ChakraProvider>
-        </ThemeProvider>
     </React.StrictMode>
 );
